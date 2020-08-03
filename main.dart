8641137ab1b1c051eb008c94fba9f78cae3e4f8e
Bhavyamaddukuri <57695980+Bhavyamaddukuri@users.noreply.github.com>
2020-08-03 22:37:21 +0530

    Add files via upload

---
+++ main.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+import 'task1/image.dart';
+import 'task1/video.dart';
+
+main() => runApp(MyApp());
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return myvideo();
+  }
+}
